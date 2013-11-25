4a6e2dd52ff1d4b809a16f429080a6af1d3940bb
今江健悟 <kengo@imaekengo-no-MacBook-Air.local>
2013-11-25 19:38:55 +0900

    Tableのpicture()メソッド以外終了

--- PrimeMinistersByJava/primeministers/Table.java
+++ PrimeMinistersByJava/primeministers/Table.java
@@ -35,7 +35,7 @@ public class Table extends Object
 	private ArrayList<Tuple> tuples;
 	
 	/**
-	 * テーブルのコンストラクタ。
+	 * テーブルのコンストラクタ。 (OK)
 	 */
 	public Table()
 	{
@@ -46,7 +46,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * タプルを追加する。
+	 * タプルを追加する。 (OK)
 	 */
 	public void add(Tuple aTuple)
 	{
@@ -55,7 +55,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * 属性リストを応答する。
+	 * 属性リストを応答する。 (OK)
 	 */
 	public Attributes attributes()
 	{
@@ -63,7 +63,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * 属性リストを設定する。
+	 * 属性リストを設定する。 (OK)
 	 */
 	public void attributes(Attributes instanceOfAttributes)
 	{
@@ -72,7 +72,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * 画像群を応答する。
+	 * 画像群を応答する。 (OK)
 	 */
 	public ArrayList<BufferedImage> images()
 	{
@@ -80,7 +80,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * 画像またはサムネイル画像の文字列を受け取って該当画像を応答する。
+	 * 画像またはサムネイル画像の文字列を受け取って該当画像を応答する。 (NO)
 	 */
 	private BufferedImage picture(String aString)
 	{
@@ -88,7 +88,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * サムネイル画像群を応答する。
+	 * サムネイル画像群を応答する。 (OK)
 	 */
 	public ArrayList<BufferedImage> thumbnails()
 	{
@@ -96,7 +96,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * 自分自身を文字列にして、それを応答する。
+	 * 自分自身を文字列にして、それを応答する。 (OK)
 	 */
 	public String toString(){
 		StringBuffer aBuffer = new StringBuffer();
@@ -106,7 +106,7 @@ public class Table extends Object
 	}
 	
 	/**
-	 * タプル群を応答する。
+	 * タプル群を応答する。 (OK)
 	 */
 	public ArrayList<Tuple> tuples(){
 		return this.tuples;
