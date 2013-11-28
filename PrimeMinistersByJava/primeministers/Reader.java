f22d5646a1abc070428a05eebd482ebbc8585ac1
今江健悟 <kengo@imaekengo-no-MacBook-Air.local>
2013-11-28 18:35:26 +0900

    Reader table()

--- PrimeMinistersByJava/primeministers/Reader.java
+++ PrimeMinistersByJava/primeministers/Reader.java
@@ -1,7 +1,11 @@
 package primeministers;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 /**
  * リーダ：総理大臣の情報を記したCSVファイルを読み込んでテーブルに仕立て上げる。
@@ -40,13 +44,41 @@ public class Reader extends IO
 	 * ダウンロードしたCSVファイルのローカルなファイルを応答するクラスメソッド。
 	 */
 	public static File filenameOfCSV(){
-		return this.filename;
+		return null;
 	}
 	
 	/**
 	 * ダウンロードしたCSVファイルを読み込んでテーブルを応答する。
 	 */
 	public Table table(){
+		
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(this.filename));
+			
+			// 最終行まで読み込む
+			String line = "";
+			while ((line = br.readLine()) != null) {
+				
+				// 1行をデータの要素に分割
+				StringTokenizer token = new StringTokenizer(line, ",");
+				
+				while (token.hasMoreTokens()) {
+					// 1行の各要素をタブ区切りで表示
+					System.out.print(token.nextToken() + "\t");
+				}
+				System.out.println();
+			}
+			br.close();
+			
+		} catch (FileNotFoundException fileNotFoundException) {
+			// Fileオブジェクト生成時の例外捕捉
+			fileNotFoundException.printStackTrace();
+		} catch (IOException iOException) {
+			// BufferedReaderオブジェクトのクローズ時の例外捕捉
+			iOException.printStackTrace();
+		}
+		
 		return null;
 	}
 }
