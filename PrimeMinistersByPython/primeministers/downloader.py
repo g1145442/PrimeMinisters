05e438c25ca06a110024685ab7ca0dcb54659c8f
TasukuTakahashi <tasukutakahashi@MacBook-Pro-for-TasukuTakahashi-2.local>
2013-12-17 11:24:29 +0900

    Makefileの修正とDownloaderの細かい部分の改善

--- PrimeMinistersByPython/primeministers/downloader.py
+++ PrimeMinistersByPython/primeministers/downloader.py
@@ -35,7 +35,6 @@ class Downloader(io.IO):
 		
 		a_table=a_reader.table()
 		
-		#ここから下使えたら後いけると思うbyゆーた
 		images_names=a_table.image_filenames()
 		self.download_images(images_names)
 		
@@ -47,36 +46,31 @@ class Downloader(io.IO):
 	def download_csv(self):
 		"""総理大臣の情報を記したCSVファイルをダウンロードする。"""
 		try: 
-			a_filename = urllib.urlopen(self._url + self._filename_of_csv)
+			a_url = urllib.urlopen(self._url + self._filename_of_csv)
 		except URLError, an_exception:  
 			print an_exception.reason
 		try: 
 			with open(self._base_directory + self._filename_of_csv ,'w') as local_file:
-					shutil.copyfileobj(a_filename, local_file)
+					shutil.copyfileobj(a_url, local_file)
 		except IOError, an_exception:
 			print an_exception.reason
-		a_filename.close()
+		a_url.close()
 		return None
 
 	def download_images(self, image_filenames):
 		"""画像ファイル群または縮小画像ファイル群をダウンロードする。"""
-# 		http://www.cc.kyoto-su.ac.jp/~atsushi/Programs/CSV2HTML/PrimeMinisters/images/039.jpg
-
-		print image_filenames
-
-		for file_name in image_filenames:
+		for filename in image_filenames:
 			try:
-				a_filename = urllib.urlopen(self._url + file_name)
+				a_url = urllib.urlopen(self._url + filename)
+				print self._url + filename
 			except URLError, an_exception:
 				print an_exception.reason
 			try:
-				with open(self._base_directory + file_name,'w') as local_file:
-						shutil.copyfileobj(a_filename, local_file)
+				with open(self._base_directory + filename,'w') as local_file:
+						shutil.copyfileobj(a_url, local_file)
 			except IOError, an_exception:
 					print an_exception.reason
-			a_filename.close()
-
-
+			a_url.close()
 		a_reader = reader.Reader(self._filename_of_csv)
 		
 		return
